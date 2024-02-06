9ee665700d4da896dc2c6b67c00e12085b1142f9
Tom Fahy <thomas.fahy9@gmail.com>
2024-02-06 21:23:15 +0000

    add wrapper function for drag and drop list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useCallback } from "react";
 import "./App.css";
+import { DragDropContext } from "react-beautiful-dnd";
+import initialData from "./assets/initialdata";
 
 function App() {
-  return <p>Wellness App</p>;
+  const onBeforeCapture = useCallback(() => {
+    /*...*/
+  }, []);
+  const onBeforeDragStart = useCallback(() => {
+    /*...*/
+  }, []);
+  const onDragStart = useCallback(() => {
+    /*...*/
+  }, []);
+  const onDragUpdate = useCallback(() => {
+    /*...*/
+  }, []);
+  const onDragEnd = useCallback(() => {
+    // the only one that is required
+  }, []);
+  return (
+    <DragDropContext
+      onBeforeCapture={onBeforeCapture}
+      onBeforeDragStart={onBeforeDragStart}
+      onDragStart={onDragStart}
+      onDragUpdate={onDragUpdate}
+      onDragEnd={onDragEnd}
+    >
+      <div>Wellness App</div>
+    </DragDropContext>
+  );
 }
 
 export default App;
