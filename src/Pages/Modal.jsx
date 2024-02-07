4804939d7dc95e7317cdd630f0cb3450c6e5c37a
sebswit <sebastian.switakowski@gmail.com>
2024-02-07 20:07:50 +0000

    Added YT player with modal

---
+++ src/Pages/Modal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Modal.css";
+import Player from "./Player";
+
+export default function Modal() {
+  return (
+    <div className="Modal">
+      <h1>Youtube Embed</h1>
+      <Player />
+    </div>
+  );
+}
