ac36e0edf305ebbfbdbb1bef156d7a5282da1ae0
Tom Fahy <thomas.fahy9@gmail.com>
2024-02-07 20:10:06 +0000

    add droppable column

---
+++ src/Components/Column.jsx
@@ -0,0 +1,32 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { useCallback } from "react";
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: 8,
+  height: "95vh",
+  width: 250,
+});
+
+export default function Board() {
+  const onDragEnd = useCallback(() => {
+    // the only one that is required
+  }, []);
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="monday" type="EXERCISE">
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+            {...provided.droppableProps}
+          >
+            <h2>Monday</h2>
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+}
