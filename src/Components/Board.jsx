1f770c92a41134695f375daafd632443e7794baa
Tom Fahy <thomas.fahy9@gmail.com>
2024-02-09 18:33:49 +0000

    add more dummy data and add state to Board component

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import uuid from "uuid/v4";
 
-const columnsFromBackend = {};
+const itemsData = [
+  { id: uuid(), content: "Exercise 1" },
+  { id: uuid(), content: "Exercise 2" },
+];
+
+const columnsData = {
+  [uuid()]: {
+    name: "Monday",
+    items: items,
+  },
+};
 
 export default function Board() {
+  const [columns, setColumns] = useState(columnsData);
   return (
     <div className="container-fluid">
       <div className="row"></div>
