7ab3852194fcdd482927326e404560b78e9a29bd
Tom Fahy <thomas.fahy9@gmail.com>
2024-02-09 19:07:00 +0000

    add droppable and draggable components

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -10,18 +10,73 @@ const itemsData = [
 const columnsData = {
   [uuid()]: {
     name: "Monday",
-    items: items,
+    items: itemsData,
   },
 };
 
 export default function Board() {
   const [columns, setColumns] = useState(columnsData);
+
   return (
     <div className="container-fluid">
       <div className="row"></div>
-      <DragDropContext
-        onDragEnd={(result) => console.log(result)}
-      ></DragDropContext>
+      <DragDropContext onDragEnd={(result) => console.log(result)}>
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <Droppable droppableID={id} key={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? "lightblue"
+                        : "green",
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500,
+                    }}
+                  >
+                    {column.items.map((item, index) => {
+                      return (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}
+                        >
+                          {(provided, snapshot) => {
+                            return (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                style={{
+                                  userSelect: "none",
+                                  padding: 16,
+                                  margin: "0 0 8px 0",
+                                  minHeight: "50px",
+                                  backgroundColor: snapshot.isDragging
+                                    ? "green"
+                                    : "blue",
+                                  color: "white",
+                                  ...provided.draggableProps.style,
+                                }}
+                              >
+                                {item.content}
+                              </div>
+                            );
+                          }}
+                        </Draggable>
+                      );
+                    })}
+                  </div>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
