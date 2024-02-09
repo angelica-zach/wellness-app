f65e2b097b8bff55aea89fe5397b2b96c555b887
angelica-zach <angelscakeunicorn@gmail.com>
2024-02-09 19:11:32 +0000

    skin care list + local storage + nanoid package

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navigation";
+import ListDisplay from "./components/skincarelist";
 function App() {
   return (
     <Router>
       <Navbar />
+      <ListDisplay />
       {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="meals" element={<MealIdeas />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
-
-import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
->
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
