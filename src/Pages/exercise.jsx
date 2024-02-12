46770770c421fc2bf1ea938aeaecff79ebc3900a
sebswit <sebastian.switakowski@gmail.com>
2024-02-12 21:03:13 +0000

    exerciseStack added to exrcise page

--- src/Pages/exercise.jsx
+++ src/Pages/exercise.jsx
@@ -1,9 +1,13 @@
 import Board from "../components/Board";
+import ExerciseStack from "./Workout";
 
 export default function Exercise() {
   return (
     <div>
+        <>
       <Board />
+        <ExerciseStack />
+      </>
     </div>
   );
 }
