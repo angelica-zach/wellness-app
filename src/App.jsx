5cc06731fa8cfdf113897a19e53b02080fbed461
Clive Makongo <clivemakongo@gmail.com>
2024-02-13 19:21:35 +0000

    added import statements for React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Meal from "./Pages/Meal";
 import "./App.css";
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navigation";
 import Home from "./Pages/Home";
