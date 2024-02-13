4dbbc83fd85d872de7611386a6a4922fa652abc4
Clive Makongo <clivemakongo@gmail.com>
2024-02-13 19:26:45 +0000

    changed file paths for pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        { <Route path="meals" element={<Meal/>} /> }
+        <Route path="meals" element={<Meal/>} />
         <Route path="workout" element={<Exercise />} />
         <Route path="selfcare" element={<Selfcare />} />
       </Routes>
